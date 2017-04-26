b71dbbb8133ea1f9b96160e3e8ff9c25a06fe39e
masterUNG <phrombutr@gmail.com>
2017-04-26 10:43:04 +0700

    Start 26April

--- app/src/main/java/appewtc/masterung/mycph/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mycph/MainActivity.java
@@ -2,12 +2,32 @@ package appewtc.masterung.mycph;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initial View
+        initialView();
+
+    }   // Main Method
+
+    private void initialView() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtNewRegis);
+        button = (Button) findViewById(R.id.btnLogin);
     }
-}
+
+}   // Main Class นี่คือ คลาสหลัก เว้ยเห้ย
