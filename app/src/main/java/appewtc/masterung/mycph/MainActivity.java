fdbe58a3c8fd5e1479f98b79c27f9f6fc27af3ea
masterUNG <phrombutr@gmail.com>
2017-04-26 12:45:35 +0700

    After Lunch

--- app/src/main/java/appewtc/masterung/mycph/MainActivity.java
+++ app/src/main/java/appewtc/masterung/mycph/MainActivity.java
@@ -1,12 +1,14 @@
 package appewtc.masterung.mycph;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Explicit
     private EditText userEditText, passwordEditText;
@@ -21,7 +23,15 @@ public class MainActivity extends AppCompatActivity {
         //Initial View
         initialView();
 
+        //Controller
+        controller();
+
     }   // Main Method
+
+    private void controller() {
+        textView.setOnClickListener(MainActivity.this);
+        button.setOnClickListener(MainActivity.this);
+    }
 
     private void initialView() {
         userEditText = (EditText) findViewById(R.id.edtUser);
@@ -30,5 +40,22 @@ public class MainActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.btnLogin);
     }
 
+
+    @Override
+    public void onClick(View view) {
+
+        //For TextView
+        if (view == textView) {
+            //Intent to SignUp
+            Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+            startActivity(intent);
+        }
+
+        //For Button
+        if (view == button) {
+
+        }
+
+    }
 
 }   // Main Class นี่คือ คลาสหลัก เว้ยเห้ย
