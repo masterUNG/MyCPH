e0af3afeafcfd858865ae75d16c2fbfd14854bcc
masterUNG <phrombutr@gmail.com>
2017-04-27 16:35:44 +0700

    Finish 27April

--- app/src/main/java/appewtc/masterung/mycph/ServiceActivity.java
+++ app/src/main/java/appewtc/masterung/mycph/ServiceActivity.java
@@ -1,18 +1,22 @@
 package appewtc.masterung.mycph;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ServiceActivity extends AppCompatActivity {
 
     private TextView textView;
     private ImageView imageView;
     private ListView listView;
-    private String[] loginStrings, nameStrings, dateStrings, detailStrings;
+    private String[] loginStrings, nameStrings,
+            dateStrings, detailStrings, qrCodeStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,25 @@ public class ServiceActivity extends AppCompatActivity {
             getData.execute(urlPHP);
             String strJSON = getData.get();
             Log.d(tag, "JSON ==> " + strJSON);
+
+            JSONArray jsonArray = new JSONArray(strJSON);
+            nameStrings = new String[jsonArray.length()];
+            dateStrings = new String[jsonArray.length()];
+            detailStrings = new String[jsonArray.length()];
+            qrCodeStrings = new String[jsonArray.length()];
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                nameStrings[i] = jsonObject.getString("Name");
+                dateStrings[i] = jsonObject.getString("Date_Receive");
+                detailStrings[i] = jsonObject.getString("Description");
+                qrCodeStrings[i] = jsonObject.getString("QR_code");
+
+            }
+
+            MyAdapter myAdapter = new MyAdapter(ServiceActivity.this, nameStrings,
+                    dateStrings, detailStrings);
+            listView.setAdapter(myAdapter);
 
 
         } catch (Exception e) {
